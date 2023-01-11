e9b2806b219f0eb13cf574b1140f5b00f921b3db
Patrick Ji <jpch89@outlook.com>
2023-01-12 07:22:10 +0800

    add files

--- jpa-hibernate-01/src/main/java/tech/tuanzi/pojo/Customer.java
+++ jpa-hibernate-01/src/main/java/tech/tuanzi/pojo/Customer.java
@@ -1,4 +1,61 @@
-package tech.tuanzi.pojo;/**
+package tech.tuanzi.pojo;
+
+import javax.persistence.*;
+
+/**
  * @author Patrick Ji
- */public class Customer {
+ */
+@Entity  // 将本类作为 Hibernate 的实体类
+@Table(name = "tb_customer")  // 映射的数据库表名
+public class Customer {
+    /**
+     * @Id：声明主键的配置
+     * @GeneratedValue：配置主键的生成策略，strategy 的取值有以下几种。
+     * GenerationType.IDENTITY：自增，MySQL。底层数据库必须支持自动增长。
+     * GenerationType.SEQUENCE：序列，Oracle。底层数据库必须支持序列。
+     * GenerationType.TABLE：JPA 提供的一种机制，通过数据库表生成主键。
+     * GenerationType.AUTO：由程序自动地帮我们选择主键生成策略。
+     * @Column：配置属性和字段的映射关系，name 为数据库表中字段的名称。
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cust_id")
+    private Long custId;  // 客户的主键
+    @Column(name = "cust_name")
+    private String custName;
+    @Column(name = "cust_address")
+    private String custAddress;
+
+    public Long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(Long custId) {
+        this.custId = custId;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "custId=" + custId +
+                ", custName='" + custName + '\'' +
+                ", custAddress='" + custAddress + '\'' +
+                '}';
+    }
 }
--- jpa-hibernate-01/src/test/java/tech/tuanzi/test/HibernateTest.java
+++ jpa-hibernate-01/src/test/java/tech/tuanzi/test/HibernateTest.java
@@ -1,4 +1,88 @@
-package tech.tuanzi.test;/**
+package tech.tuanzi.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import tech.tuanzi.pojo.Customer;
+
+/**
  * @author Patrick Ji
- */public class HibernateTest {
+ */
+public class HibernateTest {
+    // 数据库的会话工厂
+    private SessionFactory sf;
+
+    @Before
+    public void init() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("/hibernate.cfg.xml").build();
+        // 根据服务注册类创建一个元数据资源集，同时构建元数据，并生成应用唯一的 session 工厂。
+        sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    @Test
+    public void testC() {
+        // 通过 session 可以进行持久化操作
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = new Customer();
+            customer.setCustName("团子");
+            session.save(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testR() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = session.find(Customer.class, 1L);
+            System.out.println("==========");
+            System.out.println(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testRLazy() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = session.load(Customer.class, 1L);
+            System.out.println("==========");
+            System.out.println(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testU1() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = new Customer();
+            customer.setCustId(1L);
+            customer.setCustAddress("火星");
+            // update 更新
+            // 注意：没有设置的字段会更新为 null
+            session.update(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testU2() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = new Customer();
+            customer.setCustId(1L);
+            customer.setCustName("圆子");
+            // 如果设置了 id，就更新
+            // 如果没有设置 id，就创建
+            session.saveOrUpdate(customer);
+            tx.commit();
+        }
+    }
 }
