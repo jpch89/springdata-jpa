e23f8b2f9510843e879352fe2fc21408229cb9d7
Patrick Ji <jpch89@outlook.com>
2023-01-12 10:58:59 +0800

    database access based on hibernate

--- jpa-hibernate-01/src/test/java/tech/tuanzi/test/HibernateTest.java
+++ jpa-hibernate-01/src/test/java/tech/tuanzi/test/HibernateTest.java
@@ -10,6 +10,8 @@ import org.junit.Before;
 import org.junit.Test;
 import tech.tuanzi.pojo.Customer;
 
+import java.util.List;
+
 /**
  * @author Patrick Ji
  */
@@ -82,6 +84,60 @@ public class HibernateTest {
             // 如果设置了 id，就更新
             // 如果没有设置 id，就创建
             session.saveOrUpdate(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testD() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Customer customer = new Customer();
+            customer.setCustId(2L);
+            session.remove(customer);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testRHql1() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            String hql = "FROM Customer";
+            List<Customer> resultList = session.createQuery(hql, Customer.class).getResultList();
+            System.out.println(resultList);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testRHql2() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            // 如果写成这样，会报错
+            // Legacy-style query parameters (`?`) are no longer supported; use JPA-style ordinal parameters (e.g., `?1`) instead
+            // String hql = "FROM Customer WHERE custId=?";
+            // 如果写成 ?0，会有红色波浪线提示：Query parameters are numerated starting from 1
+            String hql = "FROM Customer WHERE custId=?1";
+            List<Customer> resultList = session
+                    .createQuery(hql, Customer.class)
+                    .setParameter(1, 1L)
+                    .getResultList();
+            System.out.println(resultList);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void testRHql3() {
+        try (Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+            String hql = "FROM Customer WHERE custId=:id";
+            List<Customer> resultList = session
+                    .createQuery(hql, Customer.class)
+                    .setParameter("id", 1L)
+                    .getResultList();
+            System.out.println(resultList);
             tx.commit();
         }
     }
