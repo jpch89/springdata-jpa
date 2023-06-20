dea344a20ac25dee261c0ed1b81ca604e892c76a
Patrick Ji <jpch89@outlook.com>
2023-06-20 09:32:34 +0800

    19. 自定义操作：QueryDSL 扩展

--- springdata-jpa-02/src/test/java/tech/tuanzi/QueryDSLTest.java
+++ springdata-jpa-02/src/test/java/tech/tuanzi/QueryDSLTest.java
@@ -1,12 +1,15 @@
 package tech.tuanzi;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import tech.tuanzi.config.SpringDataJPAConfig;
+import tech.tuanzi.pojo.QCustomer;
 import tech.tuanzi.repositories.CustomerQueryDSLRepository;
+
 
 @ContextConfiguration(classes = SpringDataJPAConfig.class)
 @RunWith(SpringRunner.class)
@@ -16,6 +19,25 @@ public class QueryDSLTest {
 
     @Test
     public void test01() {
+        QCustomer customer = QCustomer.customer;
 
+        // 通过 id 查找
+        // BooleanExpression 实现了 Predicate 接口
+        BooleanExpression eq = customer.custId.eq(1L);
+        System.out.println(repository.findOne(eq));
+    }
+
+    /**
+     * 查询客户范围 in
+     * id > 大于
+     * 地址 精确
+     */
+    @Test
+    public void test02() {
+        QCustomer customer = QCustomer.customer;
+        BooleanExpression and = customer.custName.in("团子", "王五")
+                .and(customer.custId.gt(0L))
+                .and(customer.custAddress.eq("Beijing"));
+        System.out.println(repository.findAll(and));
     }
 }
